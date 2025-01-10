b1e176885dfd1120cf23452e7501f6228bbc8764
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2025-01-10 00:17:38 -0300

    Coversão de tipos de dados

--- src/dev/gab/kickstart/aula8/Main.java
+++ src/dev/gab/kickstart/aula8/Main.java
@@ -1,14 +1,22 @@
 package dev.gab.kickstart.aula8;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // boolean - true or false
-        boolean  meuBooleano = true;
+        boolean  meuBooleano;
 
-        if (meuBooleano == true) {
+        System.out.println("É falso ou verdadeiro? ");
+        Scanner sc = new Scanner(System.in);
+        meuBooleano = sc.nextBoolean();
+
+        if (meuBooleano) {
             System.out.println("É verdadeiro");
+        } else {
+            System.out.println("É falso");
         }
 
         System.out.println(meuBooleano);
